cf226ba17a8648161cf7229fcc0befdc11025e0b
Darshaka Pathirana <dpat@syn-net.org>
2020-05-10 15:40:53 +0200

    Intial commit of analog-input-to-led.ino
    The task is take an analog potentiometer input and turn a colored LED on
    depending on its inuput value.
    
    Here I added three LEDs to the digital output ports 9, 10, and 11 (and
    named them redLedPin, yellowLedPin and greenLedPin).
    
    I currently have to sensor value (aka analog potentiometer input) so I
    hard coded the value to test the state machine.

---
+++ analog-input-to-led/analog-input-to-led.ino
@@ -0,0 +1,34 @@
+int sensorPin = A0;    // select the input pin for the potentiometer
+int redLedPin = 9;      // select the pin for the red LED
+int yellowLedPin = 10;      // select the pin for the yellow LED
+int greenLedPin = 11;      // select the pin for the green LED
+
+int sensorValue = 0;  // variable to store the value coming from the sensor
+
+void setup()
+{
+  pinMode(redLedPin, OUTPUT);
+  pinMode(yellowLedPin, OUTPUT);
+  pinMode(greenLedPin, OUTPUT);
+}
+
+
+
+void loop()
+{
+  sensorValue = 171;
+
+  if (sensorValue >= 0 and sensorValue < 85) {
+    digitalWrite(redLedPin, HIGH);
+    digitalWrite(yellowLedPin, LOW);
+    digitalWrite(greenLedPin, LOW);
+  } else if (sensorValue >= 85 and sensorValue < 170) {
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(yellowLedPin, HIGH);
+    digitalWrite(greenLedPin, LOW);
+  } else {
+    digitalWrite(redLedPin, LOW);
+    digitalWrite(yellowLedPin, LOW);
+    digitalWrite(greenLedPin, HIGH);
+  }
+}
